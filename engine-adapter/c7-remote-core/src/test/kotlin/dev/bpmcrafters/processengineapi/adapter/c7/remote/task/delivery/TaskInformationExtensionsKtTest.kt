661a061426847bdfe27f24cf2fee2dfa1a17b207
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-05-20 15:19:06 +0200

    consolidate date format, fix #38

--- engine-adapter/c7-remote-core/src/test/kotlin/dev/bpmcrafters/processengineapi/adapter/c7/remote/task/delivery/TaskInformationExtensionsKtTest.kt
+++ engine-adapter/c7-remote-core/src/test/kotlin/dev/bpmcrafters/processengineapi/adapter/c7/remote/task/delivery/TaskInformationExtensionsKtTest.kt
@@ -1,14 +1,16 @@
-package dev.bpmcrafters.processengineapi.adapter.c7.embedded.task.delivery
+package dev.bpmcrafters.processengineapi.adapter.c7.remote.task.delivery
 
 import dev.bpmcrafters.processengineapi.CommonRestrictions
 import org.assertj.core.api.Assertions.assertThat
+import org.camunda.bpm.client.task.impl.ExternalTaskImpl
 import org.camunda.bpm.engine.impl.persistence.entity.IdentityLinkEntity
 import org.camunda.bpm.engine.task.IdentityLink
-import org.camunda.community.mockito.delegate.DelegateTaskFake
+import org.camunda.community.mockito.task.LockedExternalTaskFake
 import org.camunda.community.mockito.task.TaskFake
 import org.junit.jupiter.api.Test
 import java.time.Instant
 import java.util.*
+
 
 class TaskInformationExtensionsKtTest {
 
@@ -39,7 +41,6 @@ class TaskInformationExtensionsKtTest {
     assertThat(taskInformation.taskId).isEqualTo("taskId")
     assertThat(taskInformation.meta[CommonRestrictions.PROCESS_DEFINITION_ID]).isEqualTo("processDefinitionId")
     assertThat(taskInformation.meta[CommonRestrictions.PROCESS_DEFINITION_KEY]).isEqualTo("processDefinitionKey")
-    assertThat(taskInformation.meta[CommonRestrictions.ACTIVITY_ID]).isEqualTo("taskDefinitionKey")
     assertThat(taskInformation.meta[CommonRestrictions.TENANT_ID]).isEqualTo("tenantId")
     assertThat(taskInformation.meta["taskName"]).isEqualTo("name")
     assertThat(taskInformation.meta["taskDescription"]).isEqualTo("description")
@@ -54,42 +55,56 @@ class TaskInformationExtensionsKtTest {
   }
 
   @Test
-  fun `should map DelegateTask`() {
+  fun `should map LockedExternalTask`() {
     val now = Date.from(Instant.now())
 
-    var delegateTask = DelegateTaskFake("taskId")
-    delegateTask = delegateTask.withProcessDefinitionId("processDefinitionId")
-    delegateTask = delegateTask.withProcessInstanceId("processInstanceId")
-    delegateTask = delegateTask.withTenantId("tenantId")
-    delegateTask = delegateTask.withTaskDefinitionKey("taskDefinitionKey")
-    delegateTask = delegateTask.withName("name")
-    delegateTask = delegateTask.withDescription("description")
-    delegateTask = delegateTask.withAssignee("assignee")
-    delegateTask = delegateTask.withCreateTime(now)
-    delegateTask = delegateTask.withFollowUpDate(now)
-    delegateTask = delegateTask.withLastUpdated(now)
-    delegateTask.dueDate = now
-    delegateTask.addGroupIdentityLink("group-1", "CANDIDATE")
-    delegateTask.addGroupIdentityLink("group-2", "CANDIDATE")
-    delegateTask.addUserIdentityLink("user-1", "CANDIDATE")
-    delegateTask.addUserIdentityLink("user-2", "CANDIDATE")
+    var lockedTaskBuilder = LockedExternalTaskFake.builder()
+    lockedTaskBuilder = lockedTaskBuilder.processDefinitionId("processDefinitionId")
+    lockedTaskBuilder = lockedTaskBuilder.processInstanceId("processInstanceId")
+    lockedTaskBuilder = lockedTaskBuilder.tenantId("tenantId")
+    lockedTaskBuilder = lockedTaskBuilder.topicName("topicName")
+    lockedTaskBuilder = lockedTaskBuilder.id("taskId")
+    lockedTaskBuilder = lockedTaskBuilder.activityId("activityId")
+    lockedTaskBuilder = lockedTaskBuilder.activityInstanceId("activityInstanceId")
+    lockedTaskBuilder = lockedTaskBuilder.createTime(now)
 
-    val taskInformation = delegateTask.toTaskInformation()
+    val lockedTask = lockedTaskBuilder.build()
+
+    val taskInformation = lockedTask.toTaskInformation()
 
     assertThat(taskInformation.taskId).isEqualTo("taskId")
     assertThat(taskInformation.meta[CommonRestrictions.PROCESS_DEFINITION_ID]).isEqualTo("processDefinitionId")
-    assertThat(taskInformation.meta[CommonRestrictions.ACTIVITY_ID]).isEqualTo("taskDefinitionKey")
+    assertThat(taskInformation.meta[CommonRestrictions.PROCESS_INSTANCE_ID]).isEqualTo("processInstanceId")
+    assertThat(taskInformation.meta[CommonRestrictions.ACTIVITY_ID]).isEqualTo("activityId")
     assertThat(taskInformation.meta[CommonRestrictions.TENANT_ID]).isEqualTo("tenantId")
-    assertThat(taskInformation.meta["taskName"]).isEqualTo("name")
-    assertThat(taskInformation.meta["taskDescription"]).isEqualTo("description")
-    assertThat(taskInformation.meta["assignee"]).isEqualTo("assignee")
+    assertThat(taskInformation.meta["topicName"]).isEqualTo("topicName")
     assertThat(taskInformation.meta["creationDate"]).isEqualTo(now.toDateString())
-    assertThat(taskInformation.meta["followUpDate"]).isEqualTo(now.toDateString())
-    assertThat(taskInformation.meta["dueDate"]).isEqualTo(now.toDateString())
-    assertThat(taskInformation.meta["formKey"]).isNull()
-    assertThat(taskInformation.meta["candidateUsers"]).isEqualTo("user-1,user-2")
-    assertThat(taskInformation.meta["candidateGroups"]).isEqualTo("group-1,group-2")
-    assertThat(taskInformation.meta["lastUpdatedDate"]).isEqualTo(now.toDateString())
+
+  }
+
+  @Test
+  fun `should map RemoteExternalTask`() {
+    val now = Date.from(Instant.now())
+
+    val externalTask = ExternalTaskImpl()
+    externalTask.processDefinitionId = "processDefinitionId"
+    externalTask.processInstanceId = "processInstanceId"
+    externalTask.tenantId = "tenantId"
+    externalTask.topicName = "topicName"
+    externalTask.id = "taskId"
+    externalTask.activityId = "activityId"
+    externalTask.activityInstanceId = "activityInstanceId"
+    externalTask.createTime = now
+
+    val taskInformation = externalTask.toTaskInformation()
+
+    assertThat(taskInformation.taskId).isEqualTo("taskId")
+    assertThat(taskInformation.meta[CommonRestrictions.PROCESS_DEFINITION_ID]).isEqualTo("processDefinitionId")
+    assertThat(taskInformation.meta[CommonRestrictions.PROCESS_INSTANCE_ID]).isEqualTo("processInstanceId")
+    assertThat(taskInformation.meta[CommonRestrictions.ACTIVITY_ID]).isEqualTo("activityId")
+    assertThat(taskInformation.meta[CommonRestrictions.TENANT_ID]).isEqualTo("tenantId")
+    assertThat(taskInformation.meta["topicName"]).isEqualTo("topicName")
+    assertThat(taskInformation.meta["creationDate"]).isEqualTo(now.toDateString())
 
   }
 
